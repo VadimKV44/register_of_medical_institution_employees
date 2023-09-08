4850f58e32c321876d38fb622209612f8960502b
ZikFreeDKarteR44 <john.smithkarter2016@mail.ru>
2023-09-08 13:04:54 +0300

    Fix flaws in the code

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -2,7 +2,6 @@ import 'package:register_of_medical_institution_employees/local_storage/local_st
 import 'package:register_of_medical_institution_employees/models/employee_model.dart';
 
 class Repository {
-
   Future<List<Employee>> getEmployees() async {
     List<Employee>? result;
     await Future.delayed(const Duration(seconds: 2), () {
